4089f5fc8ca3584d677df4cb04cdf9373615f74b
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-12-21 07:38:38 -0800

    routes conected and journals added

--- learning_journal_basic/tests.py
+++ learning_journal_basic/tests.py
@@ -1,29 +1,15 @@
-import unittest
-
+import pytest
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'learning_journal_basic')
+@pytest.fixture
+def req():
+    the_request = testing.DummyRequest()
+    return the_request
 
 
-class FunctionalTests(unittest.TestCase):
-    def setUp(self):
-        from learning_journal_basic import main
-        app = main({})
-        from webtest import TestApp
-        self.testapp = TestApp(app)
-
-    def test_root(self):
-        res = self.testapp.get('/', status=200)
-        self.assertTrue(b'Pyramid' in res.body)
+def test_home_page_renders_file_data(req):
+    """My home page view returns data."""
+    from .views import list_page
+    response = list_page(req)
+    assert '<title>list page</title>' in response
--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,41 +1,39 @@
-from pyramid.response import Response
+"""View file for learning journal."""
+
+from pyramid.view import view_config
 import io
 import os
 
 THIS_DIR = os.path.dirname(__file__)
 
+JOURNAL = {
+    1: {'id': 1, 'title': 'Joey DeRosa Learning Journal Day12', 'date': '12/20/16', 'body': 'Today was a long day, after chasing errors and refactoring math formulas our heap was finally compleat. I wouldn\'t have thought two simple methods to be so time consuming. By the time I got to my Journal app the TAs had already left. Luckilly with the help of some class-mates the app you see before you has life.'},
+    2: {'id': 2, 'title': 'Joey DeRosa Learning Journal Day11', 'date': '12/19/16', 'body': 'Today was The day we pitched our project ideas, A lot of good ones there but I think it is obvious that TrollPy is the way to go. The data structure today was called Deque and was basically a renamed doubly linked list. Really glad that pyramid was introduced and I no longer have to trouble shoot errors that seem to just create themselves in code that was previously working.'},
+    3: {'id': 3, 'title': 'Joey DeRosa Learning Journal Day10', 'date': '12/16/16', 'body': 'Today\'s white boarding challenge proved frustrating due to the limitations of what we could use, however with most things in coding the answer seemed obvious after we solved it. As for the server the new addition s weren\'t difficult to implement and gevent is a handy new feature. Our server also has some nice new response codes to throw out.'},
+    4: {'id': 4, 'title': 'Joey DeRosa Learning Journal Day9', 'date': '12/15/16', 'body': 'Today had a lot of data structure trouble. We were on a wild goose chase trying to fix errors for functionality that wasn\'t needed. I did learn a lot about requesting files from a server and how to return them. All in all an tiring but good day.'},
+    5: {'id': 5, 'title': 'Joey DeRosa Learning Journal Day8', 'date': '12/14/16', 'body': 'Today\'s data structure assignment wasn\'t much of a challenge with the single link list to work from. The server was fairly easy too because we had accidentally worked ahead the day before, however we did run into an infinite loop error that appeared for seemingly no reason. The PuPPy meetup was awesome, unfortunately I had to leave early in order to get home at a decent hour. the Kit AI was totally a Knight Rider reference.'},
+}
 
+
+@view_config(route_name='list', renderer='templates/home.jinja2')
 def list_page(request):
     """View list page."""
-    file_path = os.path.join(THIS_DIR, 'templates', 'home.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
+    return {'title_list': JOURNAL}
 
 
+@view_config(route_name='detail', renderer='templates/entry.jinja2')
 def detail_page(request):
     """View detail page."""
-    file_path = os.path.join(THIS_DIR, 'data', 'sample.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
+    return {'title_list': JOURNAL[int(request.matchdict['id'])]}
 
 
+@view_config(route_name='create', renderer='templates/new_entry.jinja2')
 def create_page(request):
     """View create page."""
-    file_path = os.path.join(THIS_DIR, 'templates', 'new_entry.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
+    return {'title_list': JOURNAL}
 
 
+@view_config(route_name='update', renderer='templates/edit_entry.jinja2')
 def update_page(request):
     """View update page."""
-    file_path = os.path.join(THIS_DIR, 'templates', 'edit_entry.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
-
-
-def includeme(config):
-    """Add routes."""
-    config.add_view(list_page, route_name='list')
-    config.add_view(detail_page, route_name='detail')
-    config.add_view(create_page, route_name='create')
-    config.add_view(update_page, route_name='update')
+    return {'title_list': JOURNAL[int(request.matchdict['id'])]}
