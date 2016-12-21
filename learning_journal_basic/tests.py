822bad92126401984278abf1cc77f518572c2f78
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-12-21 08:19:04 -0800

    meager attempts at testing

--- learning_journal_basic/tests.py
+++ learning_journal_basic/tests.py
@@ -12,4 +12,10 @@ def test_home_page_renders_file_data(req):
     """My home page view returns data."""
     from .views import list_page
     response = list_page(req)
-    assert '<title>list page</title>' in response
+    assert 'title_list' in response
+
+def test_add_entry_page_render(req):
+    """My add entry page view returns data."""
+    from .views import create_page
+    response = create_page(req)
+    assert 'title_list' in response
